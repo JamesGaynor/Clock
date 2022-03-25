3eb95a35b2b5995314c3dc2ee2c2a73d814fcce5
jamesgaynor <james_gaynor@btinternet.com>
2022-03-25 16:45:31 +0000

    style and second hand added

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // tt
+const hand = document.querySelector(".hand");
+// set up interval of 1 second
+//increment a count value 
+//change rotation of 90 degrees to 6 
+  let count = 0
+setInterval(function(){
+    console.log(hand.style.transform)
+    if (count >=360) {count = 0}
+    hand.style.transform = `rotate(${count+=6}deg)`;
+}, 1000)
+
