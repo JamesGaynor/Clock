fc78b447f1f137cb80c20ad99f90f8cf74125034
jamesgaynor <james_gaynor@btinternet.com>
2022-03-25 17:34:29 +0000

    second and minute hand working

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 // tt
 const hand = document.querySelector(".hand");
+const minutesHand = document.querySelector(".minute-hand");
 // set up interval of 1 second
-//increment a count value 
-//change rotation of 90 degrees to 6 
-  let count = 0
-setInterval(function(){
-    console.log(hand.style.transform)
-    if (count >=360) {count = 0}
-    hand.style.transform = `rotate(${count+=6}deg)`;
-}, 1000)
+//increment a count value
+//change rotation of 90 degrees to 6
+//   let count = 0
+// setInterval(function(){
+//     console.log(hand.style.transform)
+//     if (count >=360) {count = 0}
+//     hand.style.transform = `rotate(${count+=6}deg)`;
+// }, 1000)
 
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  // console.log(seconds);
+  const secondsDegrees = (seconds / 60) * 360;
+  hand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minutesHand.style.transform = `rotate(${minsDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
